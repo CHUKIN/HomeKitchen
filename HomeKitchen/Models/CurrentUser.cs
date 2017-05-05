e849a31eaf4d3cd09052b4845b045cec70f2eac1
CHUKIN <dmitriyatamanchuk@gmail.com>
2017-05-05 22:41:18 +0300

    Add Singleton

---
+++ HomeKitchen/Models/CurrentUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeKitchen.Models
+{
+    class CurrentUser
+    {
+        private static CurrentUser instance;
+
+        User user;
+        string name;
+
+        private CurrentUser(string name)
+        {
+            KitchenContext db = new KitchenContext();
+            this.name = name;
+            user = db.Users.FirstOrDefault(i=>i.Login==name);
+        }
+
+        public User GetUser()
+        {
+            return user;
+        }
+
+        public static CurrentUser getInstance(string name)
+        {
+            if (instance == null)
+                instance = new CurrentUser(name);
+            return instance;
+        }
+    }
+}
